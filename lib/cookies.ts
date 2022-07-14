ad8568c169bc442f942cd2b5021aa6b74c737772
Stefano Vena <2668723-wetfire2k@users.noreply.gitlab.com>
2022-07-14 12:01:55 +0200

    fix su cookies.ts

--- lib/cookies.ts
+++ lib/cookies.ts
@@ -24,18 +24,6 @@ const setCookie = (cname: string, cvalue: any, exdays: number = 180) => {
     return "";
   }
 
-  function checkCookie() {
-    let user = getCookie("username");
-    if (user != "") {
-      alert("Welcome again " + user);
-    } else {
-      user = prompt("Please enter your name:", "");
-      if (user != "" && user != null) {
-        setCookie("username", user, 365);
-      }
-    }
-  }
-
   const Cookies = {
      set : setCookie,
      get : getCookie,
