7a018b611bb395de4e02c9e77c5be8285bf08295
Santino Caruso <santo.caruso@altrama.com>
2022-08-01 23:04:46 +0200

    update pm2 script update README

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,18 +1,16 @@
 module.exports = {
-    apps : [
-        {
-            name: "energie-be",
-            script: "./node_modules/.bin/next",
-            args: "start",
-            exec_mode: "cluster",
-            watch: false,
-            interpreter: "/usr/bin/yarn",
-            interpreter_args: "",
-            cwd: "/home/altrama/deploy/backend/",
-            env: {
-                "PORT": 8000,
-                "NODE_ENV": "production",
-            }
+    apps: [{
+        name: "energie-be",
+        script: "./node_modules/.bin/next",
+        args: "start",
+        exec_mode: "cluster",
+        watch: false,
+        interpreter: "/usr/bin/yarn",
+        interpreter_args: "",
+        cwd: "/var/www/backend/",
+        env: {
+            "PORT": 8000,
+            "NODE_ENV": "production",
         }
-    ]
+    }]
 }
